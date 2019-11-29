53029eab5daab8e8418f45a160a7f55634517391
Yutong Huang <yxh589@case.edu>
2019-11-28 23:01:47 -0500

    Enabled cross origin request on backend

--- backend/src/main/java/com/eecs341/backend/controller/OrderController.java
+++ backend/src/main/java/com/eecs341/backend/controller/OrderController.java
@@ -3,14 +3,12 @@ package com.eecs341.backend.controller;
 import com.eecs341.backend.service.OrderService;
 import com.eecs341.backend.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/order")
 public class OrderController {
--- backend/src/main/java/com/eecs341/backend/controller/ProductSupplierController.java
+++ backend/src/main/java/com/eecs341/backend/controller/ProductSupplierController.java
@@ -3,11 +3,9 @@ package com.eecs341.backend.controller;
 import com.eecs341.backend.service.ProductSupplierService;
 import com.eecs341.backend.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/productSupplier")
 public class ProductSupplierController {
