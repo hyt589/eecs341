b1e8e235f2c45eabd60f3c7da40bfde11de80cb6
Yutong Huang <yxh589@case.edu>
2019-11-28 21:04:52 -0500

    added product component; formatted code

--- frontend/src/components/page.js
+++ frontend/src/components/page.js
@@ -1,49 +1,52 @@
-import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import displays from './display';
-import WelcomePage from './welcome';
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import WelcomePage from "./welcome";
+import OrderPage from "./order";
+import ProductPage from "./product";
 
-class Page extends React.Component{
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { display: <WelcomePage /> };
+    this.displayProductPage = this.displayProductPage.bind(this);
+    this.displayWelcomePage = this.displayWelcomePage.bind(this);
+    this.displayOrderPage = this.displayOrderPage.bind(this);
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {display: <WelcomePage/>}
-        this.displayProductPage = this.displayProductPage.bind(this)
-        this.displayWelcomePage = this.displayWelcomePage.bind(this)
-        this.displayOrderPage = this.displayOrderPage.bind(this)
-    }
+  render() {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home" onClick={this.displayWelcomePage}>
+            Inventory Management
+          </Navbar.Brand>
+          <Nav>
+            <Nav.Link href="#" onClick={this.displayProductPage}>
+              Product
+            </Nav.Link>
+            <Nav.Link href="#">Supplier</Nav.Link>
+            <Nav.Link href="#">Items</Nav.Link>
+            <Nav.Link href="#" onClick={this.displayOrderPage}>
+              Orders
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+        {this.state.display}
+      </div>
+    );
+  }
 
-    render() {
-        return (
-            <div>
-                <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand href="#home" onClick={this.displayWelcomePage}>
-                        Inventory Management
-                    </Navbar.Brand>
-                    <Nav>
-                        <Nav.Link href="#" onClick={this.displayProductPage}>Product</Nav.Link>
-                        <Nav.Link href="#">Supplier</Nav.Link>
-                        <Nav.Link href="#">Items</Nav.Link>
-                        <Nav.Link href="#" onClick={this.displayOrderPage}>Orders</Nav.Link>
-                    </Nav>
-                </Navbar>
-                {this.state.display}
-            </div>
-        )
-    }
+  displayWelcomePage() {
+    this.setState({ display: <WelcomePage /> });
+  }
 
-    displayWelcomePage (){
-        this.setState({display: <WelcomePage />})
-    }
+  displayProductPage() {
+    this.setState({ display: <ProductPage /> });
+  }
 
-    displayProductPage () {
-        this.setState({display: displays.product})
-    }
-
-    displayOrderPage (){
-        this.setState({display: displays.order})
-    }
-
+  displayOrderPage() {
+    this.setState({ display: <OrderPage /> });
+  }
 }
 
-export default Page
+export default Page;
---
+++ frontend/src/components/product.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class ProductPage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">Product</h1>
+          <p className="lead">
+            You can query product information here.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductPage
