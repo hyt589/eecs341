5205f8f046803689a94f02516012aef428695ecc
mjh244 <54736526+mjh244@users.noreply.github.com>
2019-12-13 05:51:59 -0500

    Update CustomerAccountMapper.java

--- backend/src/main/java/com/eecs341/backend/mapper/CustomerAccountMapper.java
+++ backend/src/main/java/com/eecs341/backend/mapper/CustomerAccountMapper.java
@@ -49,14 +49,23 @@ public interface CustomerAccountMapper {
 
     
 
-    @Insert("insert into eecs341.customer_account (username, email_address, shipping_address, + \n" +
+    @Select("insert into eecs341.customer_account (username, email_address, shipping_address, + \n" +
             "shipping_city, shipping_state, shipping_zip, billing_address, billing_city, + \n" +
             "billing_state, billing_zip )\n" +
-            "values (#{username}, #{email_address}, 'null', 'null', 'null'}, + \n" +
-            "'null', 'null', 'null', 'null', 'null')")
-    int insertCustomerAccount(
+            "values (#{username}, #{email_address}, #{shipping_address}, #{shipping_city}, #{shipping_state}, + \n" +
+            "#{shipping_zip}, #{billing_address}, #{billing_city}, #{billing_state}, #{billing_zip}) \n" +
+            "returning id")
+    void insertCustomerAccount(
             @Param("username") String username,
-            @Param("email_address") String email_address
+            @Param("email_address") String email_address,
+            @Param("shipping_address") String shipping_address,
+            @Param("shipping_city") String shipping_city,
+            @Param("shipping_state") String shipping_state,
+            @Param("shipping_zip") String shipping_zip,
+            @Param("billing_address") String billing_address,
+            @Param("billing_city") String billing_city,
+            @Param("billing_state") String billing_state,
+            @Param("billing_zip") String billing_zip
     );
 
     @Insert("insert into eecs341.cust_ord (customer_id, order_id)\n" +
