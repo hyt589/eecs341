adbe4828fea789d1c0bf6a226f096a91e3353329
Yutong Huang <yxh589@case.edu>
2019-12-13 09:29:15 -0500

    changed api config for deployment

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
-// const api = 'http://159.89.190.142:8080'
-const api = 'http://localhost:8080'
+const api = 'http://159.89.190.142:8080'
+// const api = 'http://localhost:8080'
 
 export default api
